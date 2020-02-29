6183f3c5f92bba84e8854a1a8de49df9726b8dc8
budicmarin <55134480+budicmarin@users.noreply.github.com>
2020-02-29 13:37:44 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace budicMArinKlaseObjektizrakoplov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Zrakoplov x380 = new Zrakoplov();
+            x380.Naziv = "Airbus";
+            x380.SnagaMotora="2000kW";
+            x380.DosegLeta = "6000km";
+            Console.WriteLine(x380.ToString);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Zrakoplov.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace budicMArinKlaseObjektizrakoplov
+{
+    class Zrakoplov
+    {
+        private string naziv;
+        private string snagaMotora;
+        private string dosegLeta;
+        public Zrakoplov()
+        {
+        }
+        public string Naziv   // property
+        {
+            get { return naziv; }   // get method
+            set { naziv = value; }  // set method
+        }
+        public string SnagaMotora
+        {
+            get { return snagaMotora; }
+            set { snagaMotora = value; }
+
+        }
+        public string DosegLeta
+        {
+            get { return dosegLeta; }
+            set { dosegLeta = value; }
+        }
+        public string ToString
+        {
+            get
+            {
+                string rec = "Naziv = " + Naziv +
+                    "\nSnaga Motora = " + SnagaMotora +
+                    "\nDoseg Leta = " + DosegLeta; ;
+                return rec;
+            }
+        }
+    }
+}
